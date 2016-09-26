bf5b375b1c30f2a83d20ae3d0df8bb5b8ad95a5a
tutysara <tutysara@gmail.com>
2016-09-27 02:43:05 +0530

    try travis ci

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+git config --global user.email "tutysara-travis-ci@gmail.com"
+git config --global user.name "tutysara"
+
+# Build the project.
+hugo -t hugo-bootstrap # if using a theme, replace by `hugo -t <yourtheme>`
+
+git clone https://github.com/tutysara/tutysara.github.com.git
+cp -R public/* ${REPO}
+cd ${REPO}
+git remote
+git add -A :/
+git commit -a -m "latest via travis"
+git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
