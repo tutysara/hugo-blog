8569e2536dc802d098368af52ceab9a4469da5c9
tutysara <tutysara-travis-ci@gmail.com>
2016-09-27 19:03:30 +0530

    change copy right content, change deploy file to use commit message

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # setup constants
 REPO="tutysara.github.com"
 GH_REPO="github.com/tutysara/${REPO}.git"
+MSG=$(git log -1 --oneline)
 git config --global user.email "tutysara-travis-ci@gmail.com"
 git config --global user.name "tutysara"
 
@@ -20,5 +21,5 @@ cp -R public/* ${REPO}
 cd ${REPO}
 git remote
 git add -A :/
-git commit -a -m "latest via travis"
+git commit -a -m "via travis -- for $MSG"
 git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
